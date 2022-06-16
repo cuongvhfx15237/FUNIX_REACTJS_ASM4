d0f2f66a8e3f5ab3402229fb89d3aee71abccb59
Vo Huy Cuong <cuongvhfx15237@funix.edu.vn>
2022-06-16 16:50:34 +0700

    fix action type

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,24 +1,17 @@
 import * as ActionTypes from './ActionTypes';
 // import {Staffs} from '../';
 import {baseUrl } from '../shared/baseUrl';
+
+
+//addStaff
 export const addStaff = (staff) => ({
-    type: ActionTypes.ADD_STAFF,
+    type: ActionTypes.ADD_STAFF_SUCCESS,
     payload: staff
 })
-export const doAddStaff = ( name, doB, startDate, departmentId, salaryScale, annualLeave, overTime ) => (dispatch) => {
-    const newStaff = {
-        name: name,
-        doB : doB,
-        startDate : startDate,
-        departmentId : departmentId,
-        salaryScale : salaryScale,
-        annualLeave:annualLeave,
-        overTime: overTime,
-    }
-    // newStaff.doB = new Date().toISOString();
+export const doAddStaff = ( staff ) => (dispatch) => {
     return fetch(baseUrl + 'staffs', {
-        method: postMessage,
-        body: JSON.stringify(newStaff),
+        method: "POST",
+        body: JSON.stringify(staff),
         headers: {
             'Content-Type': 'application/json'
         },
@@ -40,44 +33,47 @@ export const doAddStaff = ( name, doB, startDate, departmentId, salaryScale, ann
     }
     )
     .then (response => response.json())
-    .then(staffs => dispatch(doAddStaff(staffs)))
+    .then(response => dispatch(addStaff(response)))
     .catch(error => { console.log (' add new Staff', error.message)
             alert('Your infomation your send could not \n Error:' + error.message)})
 } 
-export const fetchDepartment = () => (dispatch) =>{
-    dispatch(departmentLoading(true));
+//fetch Staff
+export const fetchStaffs = () => (dispatch) =>{
+    dispatch(staffsLoading(true));
     return fetch( baseUrl + 'staffs')
-    .then(response => {
-        if (response.ok){
-            return response;
-        }
-        else {
-            var error = new Error('Error' + response.status + ':' + response.statusText)
-            error.response = response;
-            throw error;
-        }
-    },
-    error => {
-        var errmess = new Error(error.message);
-        throw errmess;
-    }
-    )
+  
     .then(response => response.json())
     .then(staffs => dispatch(doAddStaff(staffs)))
     .catch(error => dispatch(staffsFailed(error.message)))
 
 }
-export const departmentLoading = () => ({
-    type: ActionTypes.STAFF_LOADING
+export const fetchStaffsFail = () => (errmess) =>({
+   type: ActionsTypes.FETCH_STAFF_FAIL,
+   payload: errmess
+})
+
+export const fetchStaffsSuccess = () => (staffs) =>({
+    type: ActionsTypes.FETCH_STAFF_SUCCESS,
+    payload: staffs
+ })
+ export const staffsLoading = () => ({
+    type: ActionsTypes.STAFFS_LOADING,
+ })
+
+ //DELETE
+export const deleteStaffSuccess = () => (dispatch) ({
+    type: ActionTypes.DELETE_STAFF_SUCCESS,
+    payload: id
 });
-export const departmentFailed = ( errmess ) => ({
-    type: ActionTypes.STAFF_FAILED,
-    payload: errmess
+export const deleteStaffLoading = ( ) => ({
+    type: ActionTypes.DELETE_STAFF_LOADING,
 })
 
-export const modStaff = (staffModifier) => ({
-    type: ActionTypes.MOD_STAFF,
-    payload: staffModifier
-})
-
-export const fetch
+export const deleteStaff = (id) => (dispatch) =>{
+    if (confirm(" Are you sure to delete this staffs")){
+        return fetch(baseUrl + 'staffs/{id}', {
+            method: 'DELETE'
+        })
+        .then(()=> dispatch(deleteStaffSuccess(id)));
+    } else return;
+}
